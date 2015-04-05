1da2fbd342c859762a035b9961b6d3b5455533bc
yurifedoseev <fedoseev@moedelo.org>
2015-04-05 16:22:40 +0300

    refactoring main file

--- GitSelfie/GitSelfie/Program.cs
+++ GitSelfie/GitSelfie/Program.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Drawing;
-using System.IO;
-using AForge.Video;
-using AForge.Video.DirectShow;
+﻿using AForge.Video.DirectShow;
 
 namespace GitSelfie
 {
     class Program
     {
-        private static VideoCaptureDevice camera;
-        private static Commit commit;
-
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
             if (args.Length == 1 && args[0].ToLower() == "init")
             {
@@ -19,17 +12,14 @@ namespace GitSelfie
                 return;
             }
 
-            if (args.Length >= 2)
+            if (args.Length < 2)
             {
-                commit = LoadCommitData(args);
+                return;
             }
 
-            var webcamColl = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            camera = new VideoCaptureDevice(webcamColl[0].MonikerString);
-            camera.NewFrame += Device_NewFrame;
-
-            Console.WriteLine("Taking git-selfie :)");
-            camera.Start();
+            var commit = LoadCommitData(args);
+            var command = new SelfieCommand(commit);
+            command.Execute();
         }
 
         private static Commit LoadCommitData(string[] args)
@@ -46,40 +36,6 @@ namespace GitSelfie
             }
 
             return data;
-        }
-
-        static void Device_NewFrame(object sender, NewFrameEventArgs e)
-        {
-            Bitmap bmp = (Bitmap)e.Frame.Clone();
-            camera.SignalToStop();
-            CommitDrawing.Draw(bmp, commit);
-     
-            SaveImage(bmp);
-        }
-
-        private static void SaveImage(Bitmap bmp)
-        {
-            string savePath = GetFileNameToSave();
-            bmp.Save(savePath);
-        }
-
-        private static string GetFileNameToSave()
-        {
-            string folderPath = GetSaveFolderPath();
-            string fileName = DateTime.Now.ToString("yyyy_MM_dd_hhmmss") + ".png";
-            return Path.Combine(folderPath, fileName);
-        }
-        
-        private static string GetSaveFolderPath()
-        {
-            string myPicturesPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
-            const string folderName = "git-selfie";
-            string targetFolder = Path.Combine(myPicturesPath, folderName);
-            if (Directory.Exists(targetFolder) == false)
-            {
-                Directory.CreateDirectory(targetFolder);
-            }
-            return targetFolder;
         }
     }
 }
