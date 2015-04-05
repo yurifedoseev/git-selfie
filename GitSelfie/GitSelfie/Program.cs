097310e907c66c42bdac197e2e4ccc9f740f07fd
yurifedoseev <fedoseev@moedelo.org>
2015-04-05 16:29:56 +0300

    split big program.cs to small files

--- GitSelfie/GitSelfie/Program.cs
+++ GitSelfie/GitSelfie/Program.cs
@@ -1,41 +1,12 @@
-﻿using AForge.Video.DirectShow;
-
-namespace GitSelfie
+﻿namespace GitSelfie
 {
-    class Program
+    internal class Program
     {
-       static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            if (args.Length == 1 && args[0].ToLower() == "init")
-            {
-                PostCommitHook.Initialize();            
-                return;
-            }
-
-            if (args.Length < 2)
-            {
-                return;
-            }
-
-            var commit = LoadCommitData(args);
-            var command = new SelfieCommand(commit);
-            command.Execute();
-        }
-
-        private static Commit LoadCommitData(string[] args)
-        {
-            var data = new Commit
-            {
-                Message = args[0],
-                Sha1 = args[1]
-            };
-
-            if (data.Sha1.Length > 20)
-            {
-                data.Sha1 = data.Sha1.Substring(0, 20);
-            }
-
-            return data;
+            var command = CommandFactory.Create(args);
+            if (command != null)
+                command.Execute();
         }
     }
 }
